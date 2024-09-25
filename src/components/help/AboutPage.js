1d23205c73bd846ebedd6f8756a1e91c1e33edc9
raphael <raphael.zeich+gh@softwareag.com>
2024-09-25 14:56:17 +0200

    chore: :art: Replace branding to reflect the EuProGigant style

--- src/components/help/AboutPage.js
+++ src/components/help/AboutPage.js
@@ -6,9 +6,25 @@ class AboutPage extends Component {
   }
 
   render() {
-    return <div>
-      <h1>About the project</h1>
-    </div>
+    return (
+      <div>
+        <h1>About the project EuProGigant</h1>
+        EuProGigant stands for “European Production Giganet for calamity
+        avoiding self-orchestration of value chain and learning ecosystems“, the
+        binational research project for smart and sovereign use of data in the
+        European manufacturing industry. The project is carried out by an
+        Austrian-German project consortium led by the TU Wien and TU Darmstadt,
+        the goal of building a multi-location, digitally networked production
+        ecosystem. As a lightouse project for the Gaia-X initiative, which
+        strives to build an open, European data infrastructure, EuProGigant
+        advances a resilient, data-driven and sustainable industry in Europe. In
+        the ecosystem, which consists of data and infrastructure ecosystems, we
+        show how to practically realize added value for customers and
+        manufacturing companies through value creation based on smart and
+        sovereign use of data. This strengthens European industry and drives the
+        contribution of industry to the sustainable development of Europe.
+      </div>
+    );
   }
 }
 
