defbe349ad30bb93e1fa28296fedd3b10c36f281
raphael <raphael.zeich+gh@softwareag.com>
2024-09-25 16:14:08 +0200

    feat: :sparkles: Display a mobile menu on small screens

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import LanguageSelector from 'components/languageSelector/LanguageSelector';
 import { AuthContext } from 'context/AuthContextProvider';
 import { useNavbar } from 'hooks/useNavbar';
 import i18n from 'i18n';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link, NavLink } from 'react-router-dom';
 
@@ -15,15 +15,33 @@ export default function Navbar() {
   const { t } = useTranslation();
   const navbarAssets = useNavbar();
   const authContext = useContext(AuthContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobileMenu, setIsMobileMenu] = useState(false);
 
   const changeLanguage = (languageCode: string) => {
     i18n.changeLanguage(languageCode);
     localStorage.setItem('appLanguage', languageCode);
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileMenu(window.innerWidth <= 1260);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <nav className={styles.navbar}>
       <div className={styles['navbar-container']}>
+        <button className={styles['hamburger-menu']} onClick={toggleMenu}>
+          &#9776;
+        </button>
         <div className={styles.logo}>
           <Link to='/'>
             <img
@@ -33,9 +51,13 @@ export default function Navbar() {
             />
           </Link>
         </div>
-        {authContext.isAuthenticated && (
-          <ul className={styles['navigation-items']}>
-            {navbarAssets.map((asset, index) => (
+        <ul
+          className={`${styles['navigation-items']} ${
+            isMenuOpen ? styles['open'] : ''
+          }`}
+        >
+          {authContext.isAuthenticated &&
+            navbarAssets.map((asset, index) => (
               <li key={index} className={styles['navigation-item']}>
                 <NavLink
                   to={asset.path}
@@ -49,19 +71,36 @@ export default function Navbar() {
                 </NavLink>
               </li>
             ))}
-          </ul>
-        )}
-        {!authContext.isAuthenticated ? (
-          <GaiaXButton
-            label={t('top-menu.signin')}
-            handleOnClick={authContext.login}
-          />
-        ) : (
-          <GaiaXButton
-            label={t('top-menu.signout')}
-            handleOnClick={authContext.logout}
-          />
-        )}
+          {isMobileMenu &&
+            (authContext.isAuthenticated ? (
+              <GaiaXButton
+                label={t('top-menu.signout')}
+                handleOnClick={authContext.logout}
+              />
+            ) : (
+              <GaiaXButton
+                label={t('top-menu.signin')}
+                handleOnClick={authContext.login}
+              />
+            ))}
+        </ul>
+        <div className={styles['navbar-right']}>
+          {!isMobileMenu && (
+            <div className={styles['desktop-auth-button']}>
+              {!authContext.isAuthenticated ? (
+                <GaiaXButton
+                  label={t('top-menu.signin')}
+                  handleOnClick={authContext.login}
+                />
+              ) : (
+                <GaiaXButton
+                  label={t('top-menu.signout')}
+                  handleOnClick={authContext.logout}
+                />
+              )}
+            </div>
+          )}
+        </div>
         <LanguageSelector
           currentLanguage={i18n.language}
           changeLanguage={changeLanguage}
