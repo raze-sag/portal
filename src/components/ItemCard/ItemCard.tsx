a95845d75295ecbba81c3605ae1a9bd91a5d7f4c
raphael <raphael.zeich+gh@softwareag.com>
2024-09-30 11:02:48 +0200

    feat: :sparkles: Display icons according to the icon card type

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -1,25 +1,32 @@
+import HubIcon from '@mui/icons-material/Hub';
+import InterestsIcon from '@mui/icons-material/Interests';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
+import StorageIcon from '@mui/icons-material/Storage';
+import VerifiedIcon from '@mui/icons-material/Verified';
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Ontology } from 'types/ontologies.model';
+import { Resource } from 'types/resources.model';
+import { ServiceOffering } from 'types/serviceOfferings.model';
+import { Shape } from 'types/shapes.model';
 
-import { Ontology } from '../../types/ontologies.model';
-import { Resource } from '../../types/resources.model';
-import { ServiceOffering } from '../../types/serviceOfferings.model';
-import { Shape } from '../../types/shapes.model';
 import Title from '../Title/Title';
 
 import styles from './ItemCard.module.css';
 import OntologyCardContent from './OntologyCardContent';
+import ParticipantCardContent from './ParticipantCardContent';
 import ResourceCardContent from './ResourceCardContent';
 import ServiceCardContent from './ServiceCardContent';
 import ShapeCardContent from './ShapeCardContent';
 
 interface IItemCard {
-    label: string;
-    isGaiaXCompliant?: boolean;
-    ontology?: Ontology;
-    shape?: Shape;
-    service?: ServiceOffering
-    resource?: Resource
+  label: string;
+  isGaiaXCompliant?: boolean;
+  ontology?: Ontology;
+  shape?: Shape;
+  service?: ServiceOffering;
+  resource?: Resource;
 }
 
 const ItemCard: FC<IItemCard> = ({
@@ -28,20 +35,47 @@ const ItemCard: FC<IItemCard> = ({
   ontology,
   shape,
   service,
-  resource
+  resource,
 }) => {
   const { t } = useTranslation();
+
+  const getIcon = () => {
+    if (service) {
+      return (
+        <>
+          <SettingsSuggestIcon style={{ fontSize: 66, color: 'white' }} />
+        </>
+      );
+    } else if (resource) {
+      return (
+        <>
+          <StorageIcon style={{ fontSize: 66, color: 'white' }} />
+        </>
+      );
+    } else if (ontology) {
+      return <HubIcon style={{ fontSize: 66, color: 'white' }} />;
+    } else if (shape) {
+      return <InterestsIcon style={{ fontSize: 66, color: 'white' }} />;
+    } else {
+      return <QuestionMarkIcon style={{ fontSize: 66, color: 'white' }} />;
+    }
+  };
 
   return (
     <div className={styles.card}>
       <div className={styles.label}>
+        {getIcon()}
         <Title>{label}</Title>
-        {isGaiaXCompliant === undefined ? null : (
-          isGaiaXCompliant ? (
-            <p>{t('common.is-gaia-x-compliant')}</p>
-          ) : (
-            <p>{t('common.not-gaia-x-compliant')}</p>
-          )
+        {isGaiaXCompliant === undefined ? null : isGaiaXCompliant ? (
+          <p>
+            {t('common.is-gaia-x-compliant')}
+            {' '}
+            <VerifiedIcon
+              style={{ fontSize: 22, color: 'white', marginLeft: '2px' }}
+            />
+          </p>
+        ) : (
+          <p>{t('common.not-gaia-x-compliant')}</p>
         )}
       </div>
       <div className={styles.content}>
@@ -50,15 +84,15 @@ const ItemCard: FC<IItemCard> = ({
         ) : shape ? (
           <ShapeCardContent shape={shape} />
         ) : service ? (
-          <ServiceCardContent service={service}/>
+          <ServiceCardContent service={service} />
         ) : resource ? (
-          <ResourceCardContent resource={resource}/>
+          <ResourceCardContent resource={resource} />
         ) : (
           <></>
         )}
       </div>
     </div>
   );
-}
+};
 
 export default ItemCard;
