ba596bcb2cd85b0af4b7a4318abf3404e9da4c43
raphael <raphael.zeich+gh@softwareag.com>
2024-09-30 11:07:14 +0200

    feat: :sparkles: Add new Participant card

---
+++ src/components/ItemCard/ParticipantCardContent.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { Participant } from 'types/participants.model';
+
+import Title from '../Title/Title';
+import GaiaXButton from '../buttons/GaiaXButton';
+
+import styles from './ItemCard.module.css';
+
+interface IParticipantCardContent {
+  participant: Participant;
+}
+
+const ParticipantCardContent: FC<IParticipantCardContent> = ({
+  participant,
+}) => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleNavigationToDetailsPage = () => {
+    navigate(`/shapes/details/${participant.claimsGraphUri}`);
+  };
+
+  return (
+    <div className={styles.content}>
+      <div style={{ textAlign: 'left' }}>
+        <Title>{participant.legalName || t('participants.no-title')}</Title>
+      </div>
+      <p>
+        {participant.registrationNumber || t('participants.no-description')}
+      </p>
+
+      <div className={styles.button}>
+        <GaiaXButton
+          label={t('details.more-details')}
+          handleOnClick={handleNavigationToDetailsPage}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ParticipantCardContent;
